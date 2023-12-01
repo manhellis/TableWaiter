9526034e61694f8a62655cd77f60424e8520cdf8
manhellis <manh@manhellis.com>
2023-12-01 12:18:50 -0800

    updating imagery

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'all-round-gothic': ['"all-round-gothic", sans-serif'],
+      },
+      fontWeight: {
+        '200': '200', // XLig
+        '400': '400', // Book
+        '500': '500', // Medium
+        '600': '600', // Demi
+        '700': '700', // Bold
+      },
+    },
   },
   plugins: [],
 }
